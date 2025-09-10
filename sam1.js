eafdfb8fdba46c27c1566d6f6797dea48d8b0ec7
thanosda <154046417+Thanosda@users.noreply.github.com>
2025-09-10 22:45:03 +0530

    Added

---
+++ sam1.js
@@ -0,0 +1,11 @@
+array=["ani","mral" ,"bro"]
+console.log(array.length)
+
+let ab=5
+let bc=8
+let c=2
+console.log(ab+bc*2)
+
+let j=20
+let hi=j++
+console.log(hi,j)
---
+++ switch.js
@@ -0,0 +1,11 @@
+var num=20
+switch(num){
+    case 30:console.log("30");
+    break;
+    case 20:console.log("20");
+    break;
+    case 50:console.log("50");
+    break;
+    case 60:console.log("60");
+    break;
+}
