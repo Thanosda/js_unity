d40d8f050e9e91ded63ef213b6d187580d815428
thanosda <154046417+Thanosda@users.noreply.github.com>
2025-09-13 00:45:54 +0530

    Changes

--- arraysam.js
+++ arraysam.js
@@ -6,8 +6,14 @@ arr[2]="hello"
 arr[4]="hello"
 console.log(arr); */
 
-var nam="Dhivagar"
+/* var nam="Dhivagar"
 for(var i=0;i<nam.length;i++){
     console.log(nam[i]);
     
-}
+} */
+
+
+  var arr=["Dhivagar","Kumar","Muthu","Siva","Mani"]
+   var arr2=arr.slice(0,6)
+    console.log(arr2);
+     
---
+++ for ofloop.js
@@ -0,0 +1,14 @@
+/* var num="54467879473636"
+for(let val of num){
+    console.log(val)
+
+} */
+ function num2(){
+   return("5776776")
+
+ }
+let num3=num2()
+  for (let val of num3){
+    console.log(val)
+  }
+
