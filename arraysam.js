b8dca113fe0039ef43b46f7fba2e70e7f07c5bbc
thanosda <154046417+Thanosda@users.noreply.github.com>
2025-09-12 21:54:37 +0530

    Added

---
+++ arraysam.js
@@ -0,0 +1,13 @@
+/* var arr =  new Array()
+arr[0]="hello"
+arr[1]="hello"
+arr[3]="hello"
+arr[2]="hello"
+arr[4]="hello"
+console.log(arr); */
+
+var nam="Dhivagar"
+for(var i=0;i<nam.length;i++){
+    console.log(nam[i]);
+    
+}
