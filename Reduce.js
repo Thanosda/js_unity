1fe8b88d6123794b0431f83ef5f91e1475b57535
thanosda <154046417+Thanosda@users.noreply.github.com>
2025-09-13 22:47:16 +0530

    Changes

---
+++ Reduce.js
@@ -0,0 +1,4 @@
+//Also the same as the sum of array
+arr=[34,34,23,32,23,45,67,89,90,12,11,10,9,8,7,6,5,4,3,2,1]
+num=arr.reduce((a,b)=>a+b,0)
+console.log(num);
---
+++ someOf Array.js
@@ -0,0 +1,8 @@
+arr=[34,34,723,32,23,45,67,89,90,12,11,10,9,8,7,6,5,4,3,2,1]
+num=0
+for (i=0;i<arr.length;i++){
+    num=num+arr[i]
+
+
+}
+console.log(num);
